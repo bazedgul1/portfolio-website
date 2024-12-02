f0565d97c1c9a760a6283a494de12b915eb01f38
bazedgul1 <bazeed.gul@txend.com>
2024-12-02 18:59:41 +0500

    Projects Page

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -2,8 +2,19 @@
 
 const Projects = () => {
   return (
-    <div>
-      Projects Pages
+      <div className="flex flex-wrap lg:flex-nowrap justify-between px-10 py-12">
+        <div className="lg:w-1/2 w-full space-y-6 ml-40">
+            <h1 className="text-4xl font-bold tracking-tight
+              text-zinc-800 sm:text-5xl dark:text-zinc-100">Things I’ve made trying to put my dent in the universe.</h1>
+        <div>
+          <p className="text-red-200">
+          I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of.
+           Many of them are open-source, so if you see something that piques your interest, 
+           check out the code and contribute if you have ideas for how it can be improved.
+          </p>
+        </div>
+        </div>
+
     </div>
   )
 }
